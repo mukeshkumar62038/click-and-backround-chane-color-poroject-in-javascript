9918185829c80e7c767407e42f51f2d685140bb0
Mukesh kumar <mukeshsinghchiku@gmail.com>
2026-02-24 13:25:34 +0530

    click and chane backround project

---
+++ 10.js
@@ -0,0 +1,48 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'orange') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+     if (e.target.id === 'violet') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+     if (e.target.id === 'maroon') {
+      body.style.backgroundColor = e.target.id;
+    }
+     if (e.target.id === 'pink') {
+      body.style.backgroundColor = e.target.id;
+    }
+     if (e.target.id === 'magenta') {
+      body.style.backgroundColor = e.target.id;
+    }
+     if (e.target.id === 'tan') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+    
+    
+  });
+});
+
+
+
